9c4053fcc4ac0b30e20fb81a06da1a6cb14349e7
jeanatcismet <jean.ruiz@cismet.de>
2012-11-08 15:31:18 +0100

    fix #35 - ReconnectorPanel apply-button replaced by retry-button

---
+++ src/main/java/de/cismet/reconnector/ReconnectorErrorPanelWithApply.java
@@ -0,0 +1,28 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.reconnector;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public interface ReconnectorErrorPanelWithApply {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     */
+    void apply();
+}
