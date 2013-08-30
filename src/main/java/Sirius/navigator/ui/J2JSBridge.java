c2de1a7fff0c4e2876bfcfd2c58ef4487cf30dc7
Daniel Meiers <daniel.meiers@cismet.de>
2013-08-30 10:59:01 +0200

    added method to retrive all beans of class in java / js bridge

--- src/main/java/Sirius/navigator/ui/J2JSBridge.java
+++ src/main/java/Sirius/navigator/ui/J2JSBridge.java
@@ -1,58 +1,61 @@
-/**
- * *************************************************
- *
- * cismet GmbH, Saarbruecken, Germany
- * 
-* ... and it just works.
- * 
-***************************************************
- */
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 package Sirius.navigator.ui;
 
 import Sirius.navigator.connection.SessionManager;
 import Sirius.navigator.exception.ConnectionException;
+
 import Sirius.server.middleware.types.MetaClass;
 import Sirius.server.middleware.types.MetaObject;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.cismet.cids.dynamics.CidsBean;
-import de.cismet.cids.navigator.utils.ClassCacheMultiple;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
+
 import org.openide.util.Exceptions;
+
+import java.util.ArrayList;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.cids.navigator.utils.ClassCacheMultiple;
 
 /**
  * DOCUMENT ME!
  *
- * @author daniel
- * @version $Revision$, $Date$
+ * @author   daniel
+ * @version  $Revision$, $Date$
  */
 public class J2JSBridge {
 
-    public String getAllBeansForClass(final String domain, final String clazz) {
+    //~ Methods ----------------------------------------------------------------
 
-        //ToDo: checken ob tabell immer gleich Klassenname
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   domain  DOCUMENT ME!
+     * @param   clazz   DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public String getAllBeansForClass(final String domain, final String clazz) {
+        // ToDo: checken ob tabell immer gleich Klassenname
+        if ((domain == null) || (clazz == null) || domain.equals(clazz)) {
+            return "";
+        }
         final MetaClass MB_MC = ClassCacheMultiple.getMetaClass(domain, clazz);
         String query = "SELECT " + MB_MC.getID() + ", " + MB_MC.getPrimaryKey() + " ";
         query += "FROM " + MB_MC.getTableName();
         try {
             final MetaObject[] metaObjects = SessionManager.getProxy().getMetaObjectByQuery(query, 0);
-            final CidsBean[] beans = new CidsBean[metaObjects.length];
+            final ArrayList<CidsBean> beans = new ArrayList<CidsBean>();
 
-            for (int i = 0; i < metaObjects.length; i++) {
-                beans[i] = metaObjects[i].getBean();
+            for (final MetaObject mo : metaObjects) {
+                beans.add(mo.getBean());
             }
-            final ObjectMapper mapper = new ObjectMapper();
-            return mapper.writeValueAsString(beans);
+            return CidsBean.toJSONString(false, beans);
         } catch (ConnectionException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (JsonGenerationException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (JsonMappingException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
         return "";
