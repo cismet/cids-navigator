b8884d62d16202186c47bb18931411ab46ec9c05
Pascal Dihé <pascal@cismet.de>
2015-05-08 13:39:54 +0200

    missing UserService operations delegated for #140

--- src/main/java/Sirius/navigator/connection/PureRESTfulConnection.java
+++ src/main/java/Sirius/navigator/connection/PureRESTfulConnection.java
@@ -11,6 +11,7 @@ package Sirius.navigator.connection;
 
 import static Sirius.navigator.connection.RESTfulConnection.LOG;
 import Sirius.navigator.exception.ConnectionException;
+import Sirius.server.newuser.User;
 import Sirius.server.newuser.UserException;
 import Sirius.util.image.ImageHashMap;
 import java.awt.GraphicsEnvironment;
@@ -23,13 +24,11 @@ import de.cismet.cids.server.ws.rest.RESTfulSerialInterfaceConnector;
 import de.cismet.netutil.Proxy;
 
 import de.cismet.reconnector.Reconnector;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.Vector;
 import javax.swing.Icon;
 import javax.ws.rs.core.UriBuilder;
-import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 
 /**
@@ -157,12 +156,12 @@ public class PureRESTfulConnection extends RESTfulConnection {
     /**
      * FIXME: Operation currently delegated to legacy REST Connection! Implement
      * in pure RESTful Service
-     * 
+     *
      * @param username
      * @param domain
      * @return
      * @throws ConnectionException
-     * @throws UserException 
+     * @throws UserException
      */
     @Override
     public Vector getUserGroupNames(final String username, final String domain) throws ConnectionException,
@@ -180,9 +179,9 @@ public class PureRESTfulConnection extends RESTfulConnection {
     /**
      * FIXME: Operation currently delegated to legacy REST Connection! Implement
      * in pure RESTful Service
-     * 
+     *
      * @return
-     * @throws ConnectionException 
+     * @throws ConnectionException
      */
     @Override
     public ImageHashMap getDefaultIcons() throws ConnectionException {
@@ -199,20 +198,71 @@ public class PureRESTfulConnection extends RESTfulConnection {
     /**
      * FIXME: Operation currently delegated to legacy REST Connection! Implement
      * in pure RESTful Service
-     * 
+     *
      * @param name
      * @return
-     * @throws ConnectionException 
+     * @throws ConnectionException
      */
     @Override
     public Icon getDefaultIcon(final String name) throws ConnectionException {
         try {
-            LOG.warn("delegating getDefaultIcon("+name+") to legacy REST Connection");
+            LOG.warn("delegating getDefaultIcon(" + name + ") to legacy REST Connection");
             return getDefaultIcons().get(name);
         } catch (final Exception e) {
             final String message = "cannot get default icon with name '" + name + "' from from legacy REST Server"; // NOI18N
             LOG.error(message, e);
             throw new ConnectionException(message, e);
+        }
+    }
+
+    @Override
+    public boolean changePassword(final User user, final String oldPassword, final String newPassword)
+            throws ConnectionException, UserException {
+        try {
+            LOG.warn("delegating changePassword() to legacy REST Connection");
+            return this.legacyConnector.changePassword(user, oldPassword, newPassword);
+        } catch (final Exception e) {
+            final String message = "could not change password: " + user + " :: " + oldPassword + " :: " + newPassword; // NOI18N
+            LOG.error(message, e);
+            throw new ConnectionException(message, e);
+        }
+    }
+
+    /**
+     * FIXME: Operation currently delegated to legacy REST Connection! Implement
+     * in pure RESTful Service
+     * 
+     * @param user
+     * @param key
+     * @return
+     * @throws ConnectionException 
+     */
+    @Override
+    public String getConfigAttr(final User user, final String key) throws ConnectionException {
+        try {
+            LOG.warn("delegating getConfigAttr(" + user.getName() + ", " + key + ") to legacy REST Connection");
+            return this.legacyConnector.getConfigAttr(user, key);
+        } catch (final RemoteException e) {
+            throw new ConnectionException("could not get config attr for user: " + user, e); // NOI18N
+        }
+    }
+
+    /**
+     * FIXME: Operation currently delegated to legacy REST Connection! Implement
+     * in pure RESTful Service.
+     * 
+     * @param user
+     * @param key
+     * @return
+     * @throws ConnectionException 
+     */
+    @Override
+    public boolean hasConfigAttr(final User user, final String key) throws ConnectionException {
+        try {
+            LOG.warn("delegating hasConfigAttr(" + user.getName() + ", " + key + ") to legacy REST Connection");
+            return this.legacyConnector.hasConfigAttr(user, key);
+        } catch (final RemoteException e) {
+            throw new ConnectionException("could not check config attr for user: " + user, e); // NOI18N
         }
     }
 }
