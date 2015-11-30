d5fbbb42996bed7b456d5a16c49c7bfe6b8fc3b6
JeanAtCismet <jean.ruiz@cismet.de>
2015-11-30 17:08:28 +0100

    fixed #162 RightStickyToolbarItem interface added
    Toolbar-Items that implements this interface, will been shown on the right side of the MutableToolbar.
    For this purpose, a new inner toolbar has been added (rightStickyToolbar) that contains a filler component.

---
+++ src/main/java/Sirius/navigator/ui/RightStickyToolbarItem.java
@@ -0,0 +1,17 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package Sirius.navigator.ui;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public interface RightStickyToolbarItem {
+}
