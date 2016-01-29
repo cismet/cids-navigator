4d9873b6264291f9c7afd9ab0519b95d9be62091
helllth <th@cismet.de>
2016-01-29 16:55:33 +0100

    working on 167: initial stuff and minor jalopy changes

--- src/main/java/de/cismet/cids/tools/metaobjectrenderer/CoolPanel.java
+++ src/main/java/de/cismet/cids/tools/metaobjectrenderer/CoolPanel.java
@@ -69,58 +69,32 @@ public class CoolPanel extends CustomMetaObjectRenderer implements ComponentList
 
     //~ Instance fields --------------------------------------------------------
 
-    @InjectedResource
-    public int offset;
-    @InjectedResource
-    public int offsetRight;
-    @InjectedResource
-    public int offsetTop;
-    @InjectedResource
-    public int offsetBetween;
-    @InjectedResource
-    public int arcSize;
-    @InjectedResource
-    public int shadowLength;
-    @InjectedResource
-    public int blurFactor;
-    @InjectedResource
-    public float borderWidth;
-    @InjectedResource
-    public float shadowIntensity;
-    @InjectedResource
-    public float titlePanelOpacity;
-    @InjectedResource
-    public float titleLinesOpacity;
-    @InjectedResource
-    public float interPanelOpacity;
-    @InjectedResource
-    public float interLinesOpacity;
-    @InjectedResource
-    public float blurredMapOpacity;
-    @InjectedResource
-    public float cutOutMapOpacity;
-    @InjectedResource
-    public float glossyOpacity;
-    @InjectedResource
-    public Color shadowColor;
-    @InjectedResource
-    public Color colorBorder;
-    @InjectedResource
-    public Color colorMapBorder;
-    @InjectedResource
-    public Color colorTitle;
-    @InjectedResource
-    public Color colorInter;
-    @InjectedResource
-    public Color colorDarkLine;
-    @InjectedResource
-    public Color colorBrightLine;
-    @InjectedResource
-    public Color colorGlossy;
-    @InjectedResource
-    public Color gradientColorTop;
-    @InjectedResource
-    public Color gradientColorBottom;
+    @InjectedResource public int offset;
+    @InjectedResource public int offsetRight;
+    @InjectedResource public int offsetTop;
+    @InjectedResource public int offsetBetween;
+    @InjectedResource public int arcSize;
+    @InjectedResource public int shadowLength;
+    @InjectedResource public int blurFactor;
+    @InjectedResource public float borderWidth;
+    @InjectedResource public float shadowIntensity;
+    @InjectedResource public float titlePanelOpacity;
+    @InjectedResource public float titleLinesOpacity;
+    @InjectedResource public float interPanelOpacity;
+    @InjectedResource public float interLinesOpacity;
+    @InjectedResource public float blurredMapOpacity;
+    @InjectedResource public float cutOutMapOpacity;
+    @InjectedResource public float glossyOpacity;
+    @InjectedResource public Color shadowColor;
+    @InjectedResource public Color colorBorder;
+    @InjectedResource public Color colorMapBorder;
+    @InjectedResource public Color colorTitle;
+    @InjectedResource public Color colorInter;
+    @InjectedResource public Color colorDarkLine;
+    @InjectedResource public Color colorBrightLine;
+    @InjectedResource public Color colorGlossy;
+    @InjectedResource public Color gradientColorTop;
+    @InjectedResource public Color gradientColorBottom;
 
     // Lumbermill Logger initialisieren
     private final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
