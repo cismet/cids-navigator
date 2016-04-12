8c3d447988b72df999e7c5ed7008d2e5a6cd41d2
JeanAtCismet <jean.ruiz@cismet.de>
2016-04-12 11:14:13 +0200

    issue #174 connection class fixed

--- src/main/java/de/cismet/cids/client/tools/CidsAuthentification.java
+++ src/main/java/de/cismet/cids/client/tools/CidsAuthentification.java
@@ -34,10 +34,11 @@ public class CidsAuthentification extends LoginService {
 
     public static final String CONNECTION_PROXY_CLASS =
         "Sirius.navigator.connection.proxy.DefaultConnectionProxyHandler";
-    public static final String CONNECTION_CLASS = "Sirius.navigator.connection.RMIConnection";
+    public static final String CONNECTION_CLASS = "Sirius.navigator.connection.RESTfulConnection";
 
     //~ Instance fields --------------------------------------------------------
 
+    private final String connectionClass;
     private final String callServerURL;
     private final String domain;
 
@@ -50,6 +51,20 @@ public class CidsAuthentification extends LoginService {
      * @param  domain         DOCUMENT ME!
      */
     public CidsAuthentification(final String callServerURL, final String domain) {
+        this.connectionClass = CONNECTION_CLASS;
+        this.callServerURL = callServerURL;
+        this.domain = domain;
+    }
+
+    /**
+     * Creates a new CidsAuthentification object.
+     *
+     * @param  connectionClass  DOCUMENT ME!
+     * @param  callServerURL    DOCUMENT ME!
+     * @param  domain           DOCUMENT ME!
+     */
+    public CidsAuthentification(final String connectionClass, final String callServerURL, final String domain) {
+        this.connectionClass = connectionClass;
         this.callServerURL = callServerURL;
         this.domain = domain;
     }
@@ -74,11 +89,9 @@ public class CidsAuthentification extends LoginService {
         final String user = (split.length > 1) ? split[0] : name;
         final String group = (split.length > 1) ? split[1] : null;
 
-        final String connectionclass = "Sirius.navigator.connection.RMIConnection";
-
         try {
             final Connection connection = ConnectionFactory.getFactory()
-                        .createConnection(connectionclass, callServerURL);
+                        .createConnection(connectionClass, callServerURL);
             final ConnectionInfo connectionInfo = new ConnectionInfo();
             connectionInfo.setCallserverURL(callServerURL);
             connectionInfo.setPassword(new String(password));
