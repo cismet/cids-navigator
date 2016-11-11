0d297568e9f256cae272662e91018960c845e9e6
JeanAtCismet <jean.ruiz@cismet.de>
2016-11-11 18:36:16 +0100

    fixed #181

--- src/main/java/de/cismet/cids/editors/converters/BooleanToStringConverter.java
+++ src/main/java/de/cismet/cids/editors/converters/BooleanToStringConverter.java
@@ -21,22 +21,68 @@ import org.jdesktop.beansbinding.Converter;
  */
 public class BooleanToStringConverter extends Converter<Boolean, String> {
 
+    //~ Instance fields --------------------------------------------------------
+
+    private final String yes;
+    private final String no;
+    private final String nul;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new BooleanToStringConverter object.
+     */
+    public BooleanToStringConverter() {
+        this(Boolean.TRUE.toString(), Boolean.FALSE.toString(), null);
+    }
+
+    /**
+     * Creates a new BooleanToStringConverter object.
+     *
+     * @param  yes  DOCUMENT ME!
+     * @param  no   DOCUMENT ME!
+     */
+    public BooleanToStringConverter(final String yes, final String no) {
+        this(yes, no, null);
+    }
+
+    /**
+     * Creates a new BooleanToStringConverter object.
+     *
+     * @param  yes  DOCUMENT ME!
+     * @param  no   DOCUMENT ME!
+     * @param  nul  DOCUMENT ME!
+     */
+    public BooleanToStringConverter(final String yes, final String no, final String nul) {
+        this.yes = yes;
+        this.no = no;
+        this.nul = nul;
+    }
+
     //~ Methods ----------------------------------------------------------------
 
     @Override
     public String convertForward(final Boolean value) {
-        if (value != null) {
-            return value.toString();
+        if (value == null) {
+            return nul;
         } else {
-            return null;
+            return value ? yes : no;
         }
     }
 
     @Override
     public Boolean convertReverse(final String value) {
         try {
-            return new Boolean(value);
-        } catch (Exception e) {
+            if ((value == null) || value.equals(nul)) {
+                return null;
+            } else if (value.equals(yes)) {
+                return true;
+            } else if (value.equals(no)) {
+                return false;
+            } else {
+                return null;
+            }
+        } catch (final Exception e) {
             return null;
         }
     }
