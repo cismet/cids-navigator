c56ed545f2b2893265b0cf8e5477d0592e8eac7c
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-01-26 17:06:42 +0100

    issue #247 adding getConfigAttrs

--- src/main/java/Sirius/navigator/connection/Connection.java
+++ src/main/java/Sirius/navigator/connection/Connection.java
@@ -1022,9 +1022,36 @@ public interface Connection {
     String getConfigAttr(User user, String key) throws ConnectionException;
 
     /**
-     * @see  Sirius.server.middleware.interfaces.proxy.UserService#getConfigAttr(Sirius.server.newuser.User, java.lang.String)
+     * DOCUMENT ME!
+     *
+     * @param   user               DOCUMENT ME!
+     * @param   key                DOCUMENT ME!
+     * @param   connectionContext  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  ConnectionException  DOCUMENT ME!
+     *
+     * @see     Sirius.server.middleware.interfaces.proxy.UserService#getConfigAttr(Sirius.server.newuser.User, java.lang.String)
+     *
+     * @@deprecateduseListgetConfigAttr(User user, String key, ConnectionContext connectionContext) instead
      */
+
+    @Deprecated
     String getConfigAttr(User user, String key, ConnectionContext connectionContext) throws ConnectionException;
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   user               DOCUMENT ME!
+     * @param   key                DOCUMENT ME!
+     * @param   connectionContext  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  ConnectionException  DOCUMENT ME!
+     */
+    String[] getConfigAttrs(User user, String key, ConnectionContext connectionContext) throws ConnectionException;
 
     /**
      * DOCUMENT ME!
