1f1b06f149fc0873565c953e15c1578294b8a39c
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-07-06 15:53:56 +0200

    fixed #253

--- src/main/java/Sirius/navigator/config/DefaultNavigatorConfigAttrProviderImpl.java
+++ src/main/java/Sirius/navigator/config/DefaultNavigatorConfigAttrProviderImpl.java
@@ -11,8 +11,6 @@ import Sirius.navigator.connection.ConnectionSession;
 import Sirius.navigator.connection.SessionManager;
 import Sirius.navigator.exception.ConnectionException;
 
-import Sirius.server.newuser.User;
-import Sirius.server.newuser.UserGroup;
 
 import org.apache.log4j.Logger;
 
@@ -64,89 +62,18 @@ public final class DefaultNavigatorConfigAttrProviderImpl implements ConfigAttrP
 
     @Override
     public String getGroupConfigAttr(final String key) {
-        final ConnectionSession session = SessionManager.getSession();
-        if (session == null) {
-            throw new IllegalStateException("no session available"); // NOI18N
-        }
-
-        final User user = session.getUser();
-        final UserGroup userGroup = user.getUserGroup();
-        if (userGroup != null) {
-            return getGroupConfigAttr(key, user.getDomain(), userGroup);
-        } else {
-            for (final UserGroup potentialUserGroup : user.getPotentialUserGroups()) {
-                final String confAttr = getGroupConfigAttr(key, user.getDomain(), potentialUserGroup);
-                if (confAttr != null) {
-                    return confAttr;
-                }
-            }
-            return null;
-        }
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param   key         DOCUMENT ME!
-     * @param   userDomain  DOCUMENT ME!
-     * @param   userGroup   DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    private String getGroupConfigAttr(final String key, final String userDomain, final UserGroup userGroup) {
-        final ConnectionSession session = SessionManager.getSession();
-        try {
-            final User queryUser = new User(-1, "", userDomain, userGroup); // NOI18N
-
-            return session.getConnection().getConfigAttr(queryUser, key, getConnectionContext());
-        } catch (final ConnectionException e) {
-            LOG.error("could not get group config attr for key: " + key, e); // NOI18N
-            return null;
-        }
+        // there was more code here before, cycling through the potentialUsergroups
+        // and checking each group individually. This made no sense, as the server
+        // is already doing this (more efficiently)
+        return getUserConfigAttr(key);
     }
 
     @Override
     public String getDomainConfigAttr(final String key) {
-        final ConnectionSession session = SessionManager.getSession();
-        if (session == null) {
-            throw new IllegalStateException("no session available"); // NOI18N
-        }
-
-        final User user = session.getUser();
-        final UserGroup userGroup = user.getUserGroup();
-        if (userGroup != null) {
-            return getDomainConfigAttr(key, user.getDomain(), userGroup);
-        } else {
-            for (final UserGroup potentialUserGroup : user.getPotentialUserGroups()) {
-                final String confAttr = getDomainConfigAttr(key, user.getDomain(), potentialUserGroup);
-                if (confAttr != null) {
-                    return confAttr;
-                }
-            }
-            return null;
-        }
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param   key         DOCUMENT ME!
-     * @param   userDomain  DOCUMENT ME!
-     * @param   userGroup   DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    private String getDomainConfigAttr(final String key, final String userDomain, final UserGroup userGroup) {
-        final ConnectionSession session = SessionManager.getSession();
-        try {
-            final UserGroup queryUg = new UserGroup(-1, "", userGroup.getDomain()); // NOI18N
-            final User queryUser = new User(-1, "", userDomain, queryUg);           // NOI18N
-
-            return session.getConnection().getConfigAttr(queryUser, key, getConnectionContext());
-        } catch (final ConnectionException e) {
-            LOG.error("could not get domain config attr for key: " + key, e); // NOI18N
-            return null;
-        }
+        // there was more code here before, cycling through the potentialUsergroups
+        // and checking each group individually. This made no sense, as the server
+        // is already doing this (more efficiently)
+        return getUserConfigAttr(key);
     }
 
     @Override
